f05a5fe26bae9d100b255c9018fa607e7295cb8d
mathimyh <matmyhre@hotmail.no>
2023-01-31 19:59:18 +0100

    absolutt ingen framgang

--- exercise1/functions.jl
+++ exercise1/functions.jl
@@ -1,4 +1,4 @@
-## First define functions for wall collisions
+############# First define functions for wall collisions ##########################################
 vert_wall((v_x, v_y)) = (-ksi*v_x, ksi*v_y)
 hori_wall((v_x, v_y)) = (ksi*v_x, -ksi*v_y)
 function vert_delta_t(radius, x_i, v_xi)
@@ -19,12 +19,13 @@ function hori_delta_t(radius, y_i, v_yi)
         return (radius-y_i) / v_yi
     end
 end
+###################################################################################################
 
-## Then functions for two particle collisions
+############# Then functions for two particle collisions ##########################################
 delta_x((x_i, y_i),(x_j, y_j)) = (x_j-x_i, y_j-y_i)
 delta_v((v_xi, v_yi), (v_xj, v_yj)) = (v_xj-v_xi, v_yj-v_yi)
 R_squared((x_i,y_i), (x_j, y_j)) = (x_j-x_i)^2 + (y_j-y_i)^2
-d(i,j,v_i,v_j, rad_i, rad_j) = (dot(delta_v(v_i, v_j), delta_x(i,j)))^2 - (dot(delta_v(v_i, v_j), delta_v(v_i, v_j))) * (dot(delta_x(i,j), delta_x(i,j))-(rad_i+rad_j+10^-4)^2)
+d(i,j,v_i,v_j, rad_i, rad_j) = (dot(delta_v(v_i, v_j), delta_x(i,j)))^2 - (dot(delta_v(v_i, v_j), delta_v(v_i, v_j))) * (dot(delta_x(i,j), delta_x(i,j))-(rad_i+rad_j)^2)
 function delta_t(i,j,v_i,v_j, rad_i, rad_j)
     if dot(delta_v(v_i, v_j), delta_x(i,j)) >= 0
         return nothing
@@ -34,74 +35,53 @@ function delta_t(i,j,v_i,v_j, rad_i, rad_j)
         dv_dx =  dot(delta_v(v_i, v_j), delta_x(i,j))
         dd = d(i,j, v_i, v_j, rad_i, rad_j)
         dv_dv =  dot(delta_v(v_i, v_j), delta_v(v_i, v_j))
-        println("dv_dx = $dv_dv")
-        println("dd = $dd")
-        println("dv_dv = $dv_dv")
-        println(- (dv_dx + sqrt(dd)) / dv_dv)
         return - (dv_dx + sqrt(dd)) / dv_dv
     end
 end
-
 function big_factor_i(disc_i, disc_j)
     return (1+ksi) * (disc_j.mass/(disc_i.mass+disc_j.mass)) * (dot(delta_v(disc_i.vel, disc_j.vel), delta_x(disc_i.pos, disc_j.pos))/(disc_i.radius+disc_j.radius)^2)
 end
-
 function big_factor_j(disc_i, disc_j)
     return (1+ksi) * (disc_i.mass/(disc_i.mass+disc_j.mass)) * (dot(delta_v(disc_i.vel, disc_j.vel), delta_x(disc_i.pos, disc_j.pos))/((disc_i.radius+disc_j.radius)^2))
 end
-
 function vel_two_discs_i(disc_i, disc_j)
     vel = (disc_i.vel[1] + big_factor_i(disc_i, disc_j) * delta_x(disc_i.pos, disc_j.pos)[1], disc_i.vel[2] + big_factor_i(disc_i, disc_j) * delta_x(disc_i.pos, disc_j.pos)[2])
     return vel
 end
-
 function vel_two_discs_j(disc_i, disc_j)
     vel = (disc_j.vel[1] - big_factor_j(disc_i, disc_j) * delta_x(disc_i.pos, disc_j.pos)[1], disc_i.vel[2] + big_factor_j(disc_i, disc_j) * delta_x(disc_i.pos, disc_j.pos)[2])
     return vel 
 end
+##################################################################################################
 
-# The function that finds the earliest collision for a disc. Updates the queue as well
+
+##### The function that finds the earliest collision for a disc. Updates the queue as well #######
 function update_collision(disc, discs, queue, clock)
-    time::Float64 = 10^8
     crash = nothing
+    time = nothing
     for other in discs if other != disc
         temp = delta_t(disc.pos, other.pos, disc.vel, other.vel, disc.radius, other.radius)
         if !isnothing(temp)
-            if temp < time
-                time = temp
-                crash = other
-            end
-        end
-    end
-    end
-    temp = hori_delta_t(disc.radius, disc.pos[2], disc.vel[2])
-    if !isnothing(temp)
-        if temp < time
             time = temp
-            crash = HoriWall()
-        end
-    end
-    temp = vert_delta_t(disc.radius, disc.pos[1], disc.vel[1])
-    if !isnothing(temp)
-        if temp < time
-            time = temp
-            crash = VertWall()
-        end
-    end
-    if isnothing(crash)
-        return nothing
-    end
-    if (typeof(crash) != Disc)
-        enqueue!(queue, Collision(disc, crash, disc.c_count, 0, time) => time + clock.time)
-    else
-        # colls = [(coll.object1.pos, coll.object2.pos) for coll in collect(keys(queue)) if typeof(coll.object2) == Disc]
-        # if !((crash.pos, disc.pos) in colls) # Dont add the same collision twice
+            crash = other
             enqueue!(queue, Collision(disc, crash, disc.c_count, crash.c_count, time) => time + clock.time)
-        # end
-    end    
+        end
+        end
+    end
+    time = hori_delta_t(disc.radius, disc.pos[2], disc.vel[2])
+    crash = HoriWall()
+    if !isnothing(time)
+        enqueue!(queue, Collision(disc, crash, disc.c_count, 0, time) => time + clock.time)
+    end
+    time = vert_delta_t(disc.radius, disc.pos[1], disc.vel[1])
+    crash = VertWall()
+    if !isnothing(time)
+        enqueue!(queue, Collision(disc, crash, disc.c_count, 0, time) => time + clock.time)
+    end 
+    return nothing
 end
 
-# Initializes all collision at the start of the simulation given the disc array.
+##### Initializes all collision at the start of the simulation given the disc array ##############
 function initialize_collisions(discs)
     clock = Clock(0)
     queue = PriorityQueue()
@@ -112,18 +92,18 @@ function initialize_collisions(discs)
 end             
 
 
-## The function that moves the system to the next collision. 
+##### The function that moves the system to the next collision ##################################
 function update(queue, discs, clock)
     next = dequeue!(queue) 
     # Check if collision is valid first. If not try again
     if (next.object1.c_count != next.count1) 
         #update_collision(next.object1, discs, queue, clock)
-        return nothing
+        return false
     end
     if typeof(next.object2) != HoriWall && typeof(next.object2) != VertWall
         if (next.object2.c_count != next.count2)
             #update_collision(next.object2, discs, queue, clock)
-            return nothing
+            return false
         end
     end
     # Updating positions of all discs
@@ -156,8 +136,6 @@ function update(queue, discs, clock)
         update_collision(next.object2, discs, queue, clock)
         
     end
-    return nothing
+    return true
 end
     
-
-
--- exercise1/main.jl
+++ exercise1/main.jl
@@ -16,24 +16,24 @@ function circle(x, y , radius; n=30)
     Plots.Shape(radius*sind.(θ) .+ x, radius*cosd.(θ) .+ y)
 end
 
-mass_i = 1
-mass_j = 0.01
-ksi = 0
-radius = 0.05
+mass_i = 10^6
+mass_j = 
+ksi = 1
+radius = 0.04
 
 
-disc1 = Disc((0.1, 0.5), (0.4,0), mass_i, 0.05, 0)
-disc2 = Disc((0.9, 0.5), (-0.4,0), mass_i, 0.05, 0)
+disc1 = Disc((0.1, 0.5), (0.2,0), mass_i, 0.01, 0)
+disc2 = Disc((0.9, 0.5), (-0.2,0), mass_i, 0.01, 0)
 disc3 = Disc((0.7, 0.2), (0.38, 0.32), mass_i, 0.05, 0)
 discs = [disc1, disc2]#, disc3]
 # discs = []
 # for i in 1:5
-#     temp = Disc((rand(), rand()), (rand((0.0,0.4)), rand((0.0,0.4))), mass_i, radius, 0)
+#     temp = Disc((rand(), rand()), (rand((-0.4,0.4)), rand((-0.4,0.4))), mass_i, radius, 0)
 #     push!(discs, temp)
 # end
 queue, clock = initialize_collisions(discs)
 
-
+##### Function for getting a snapshot of the system ####################
 # for i in 1:7
 #     println("\n", queue)
 #     update(queue, discs, clock)
@@ -42,9 +42,12 @@ queue, clock = initialize_collisions(discs)
 
 # circles = circle.(discs)
 # plot(circles, xlim=(0,1), ylim=(0,1))
+########################################################################
 
+
+##### Function for animating the system ################################
 function plotting_easy(circles)
-    plot(circles, legend=false, xlim=(0,1), ylim=(0,1))
+    plot!(circles, legend=false, xlim=(0,1), ylim=(0,1))
     Plots.frame(anim)
 end
 
@@ -54,39 +57,50 @@ function move_til_next(queue, discs, clock, anim)
     end
     startpoints = [disc.pos for disc in discs]
     vels = [disc.vel for disc in discs]
-    println(([sqrt((vel[1])^2+(vel[2])^2) for vel in vels]))
+    #println(([sqrt((vel[1])^2+(vel[2])^2) for vel in vels]))
     moving_time = peek(queue)[1].time_until
     #println(queue, "\n")
-    update(queue, discs, clock)
+    moved = update(queue, discs, clock)
+    println(moved)
     x = []
     y = []
     radii = []
-    for i in 0:0.05:moving_time
-        circles = []
-        for j in 1:length(startpoints)
-            push!(x, startpoints[j][1] + vels[j][1]*i)
-            push!(y, startpoints[j][2] + vels[j][2]*i)
-            push!(radii, discs[j].radius)
+    if moved
+        for i in 0:0.05:moving_time
+            circles = []
+            for j in 1:length(startpoints)
+                push!(x, startpoints[j][1] + vels[j][1]*i)
+                push!(y, startpoints[j][2] + vels[j][2]*i)
+                push!(radii, discs[j].radius)
+            end
+            circles = circle.(x, y, radii)
+            plotting_easy(circles)
         end
-        circles = circle.(x, y, radii)
-        plotting_easy(circles)
     end
 end
 
 
 anim = Plots.Animation()
-for k in 1:2
+energy_start = sum([1/2 * disc.mass * (disc.pos[1]^2+disc.pos[2]^2) for disc in discs])
+for i in 1:20
+    # if i < 11
+    #     temp = copy(queue)
+    #     println("i = $i")
+    #     println("First: ", peek(temp))
+    #     dequeue!(temp)
+    #     println("Second: ", peek(temp))
+    #     dequeue!(temp)
+    #     println("Third: ", peek(temp), "\n")
+    # end
     move_til_next(queue, discs, clock, anim)
 end
+energy_end = sum([1/2 * disc.mass * (disc.pos[1]^2+disc.pos[2]^2) for disc in discs])
 
+println("Energy at start of simulation: $energy_start")
+println("Energy at end of simulation: $energy_end")
 
 gif(anim, "anim2.gif")
-
-# anim = @animate for _ in 1:steps
-#     move_til_next(queue, discs, clock)
-# end
-
-# gif(anim, "anim.gif")
+#####################################################################################
 
 
 
